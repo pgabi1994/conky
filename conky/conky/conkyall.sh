0e4d5cbfd3652adec12688ba5b44a00559b49cbe
pgabi1994 <pazmangabor94@gmail.com>
2022-01-22 05:23:21 +0100

    Delete conky directory

--- conky/conky/conkyall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sleep 5
-conky -q -c /home/dr/conky/time &
-conky -q -c /home/dr/conky/cpu &
-conky -q -c /home/dr/conky/gpu &
-conky -q -c /home/dr/conky/net &
-conky -q -c /home/dr/conky/sys &
-conky -q -c /home/dr/conky/disk & exit
